bd36cd7b911fe34a238f3e660c20af98641570f4
MhlGav <mikhail.gavrilyuk.03@mail.ru>
2023-05-06 11:26:12 +0300

    Initial commit

---
+++ mysite/pools/controllers/index.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def about(request):
+    return HttpResponse("My name is Mikhail")
+
+
+def contacts(request):
+    return HttpResponse("My contacts")
+
+def products(request):
+    return HttpResponse("Our products")
+
+def reviews(request):
+    return HttpResponse("Our reviews")
